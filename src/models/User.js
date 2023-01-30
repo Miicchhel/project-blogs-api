8428975205a30cf0559a00ce62f6eb3e5acb90cd
Michel Ferreira <michel.ferreira.melo@gmail.com>
2023-01-30 13:50:49 -0300

    feat: Criando o modelo User em src/models/User.js com as propriedades corretas

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'users',
+    }
+  );
+
+  User.associate = (models) => {
+    User.hasMany(
+      models.BlogPost,
+      { foreignKey: 'user_id' }
+    );
+  };
+
+  return User;
+};
