50413741ec0743ff19004f799fe6362c88af379b
Michel Ferreira <michel.ferreira.melo@gmail.com>
2023-01-31 04:05:59 -0300

    feat: Criando o modelo BlogPost em src/models/BlogPost.js com as propriedades e associações corretas

---
+++ src/models/BlogPost.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    'BlogPost',
+    {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'blog_posts',
+    }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(
+      models.User,
+      { foreignKey: 'user_id' }
+    );
+  };
+
+  return BlogPost;
+};
